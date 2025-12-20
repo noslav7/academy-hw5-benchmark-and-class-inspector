0e5c21dbd97b5029d7f5aae538fc648ee41ec2dd
noslav7 <noslav7@gmail.com>
2025-12-20 14:23:10 +0500

    fix: import added

--- hw5-class-inspector/src/test/java/academy/format/OutputFormatTest.java
+++ hw5-class-inspector/src/test/java/academy/format/OutputFormatTest.java
@@ -1,5 +1,6 @@
 package academy.format;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
